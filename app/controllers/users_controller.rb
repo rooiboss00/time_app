c81e10f328c3f2d3b7fda7cba451ab3edc343f76
rooiboss00 <e_au3@yahoo.co.jp>
2020-06-22 11:53:01 +0900

    ログイン構造終わり！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "こんにちは！登録に成功しました！ご登録ありがとうございます！"
       redirect_to @user
     else
