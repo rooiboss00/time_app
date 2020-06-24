5d6c985cae57a55a0e73e774638e9581ddbe3035
rooiboss00 <e_au3@yahoo.co.jp>
2020-06-24 09:52:33 +0900

    CSS反映されない

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
+
+  config.assets.debug = true#追加
+  config.assets.quiet = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
